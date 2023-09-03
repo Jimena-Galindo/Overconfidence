753a3eeb7980f0584e69059d469af019c9ecd3f2
Jimena-Galindo <jimena.galindop93@gmail.com>
2023-09-03 17:23:42 -0400

    other data and writing models

---
+++ Paper/compile_paper.R
@@ -0,0 +1,8 @@
+# install missing packages from the lockfile
+renv::restore()
+
+# paper options
+knitr::opts_chunk$set(eval=T, echo=F, warning=F, message=F, cache=F)
+
+# compile the paper
+rmarkdown::render("Paper/draft.Rmd", output_format = "pdf_document")

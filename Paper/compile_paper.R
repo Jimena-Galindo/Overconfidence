fdbd55c9792006bff84640cfdba9b34e936a9843
Jimena-Galindo <jimena.galindop93@gmail.com>
2023-10-15 16:29:41 -0400

    Smm estimation, write section and rerun

--- Paper/compile_paper.R
+++ Paper/compile_paper.R
@@ -6,3 +6,8 @@ knitr::opts_chunk$set(eval=T, echo=F, warning=F, message=F, cache=F)
 
 # compile the paper
 rmarkdown::render("Paper/draft.Rmd", output_format = "pdf_document")
+
+rmarkdown::render("Paper/Intro.Rmd", output_format = "pdf_document")
+
+
+
